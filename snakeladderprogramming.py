098611294433c55c1a46ae2ee62adbd72975f46c
17BTCSO24 <35717814+17BTCSO24@users.noreply.github.com>
2018-01-30 10:32:34 +0530

    Add files via upload

--- snakeladderprogramming.py
+++ snakeladderprogramming.py
@@ -1,16 +1,16 @@
 #program for snake and ladder
-import random
-count=0
-while(count<=100):
-    a=input("enter 'r' to roll a dice")
-    if(a=='r'):
-        r=random.randint(1,6)
-        count=count+r
-    print("Your dice value")
+import random #we are importing a random function
+count=0 #intially player position is zero where we start a game
+while(count<=100):#we are using while condition that count must be less than are equal to 100
+    a=input("enter 'r' to roll a dice")#we are taking input from the user as r
+    if(a=='r'):#a is just a variable that we use we are comparing the random variable if this statement is true then the next condition comes
+        r=random.randint(1,6)#here r is a random integer from 1 to 6
+        count=count+r#we are incrementing the count value
+    print("Your dice value")#printing a dice value
     print(r)
-    print("Your count value")
+    print("Your count value")#printing a count value
     print(count)
-    if(count==8):
+    if(count==8):#using if statement we are initiasing a count value
         count=37
         print("wow you have climbed a ladder")
     elif(count==11):
@@ -44,7 +44,7 @@ while(count<=100):
         count=64
         print("no snake has bitten you")
     else:
-        if count==100:
+        if count==100:#if count is equal to 100 you have won the game
            print("You have won the game:")
         
         
