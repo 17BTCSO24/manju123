8fccf82ee55fee808f0e7053de878861adbd9ae1
17BTCSO24 <35717814+17BTCSO24@users.noreply.github.com>
2018-02-24 10:20:41 +0530

    Add files via upload

---
+++ session1.py
@@ -0,0 +1,10 @@
+#program using for loop
+a=[1,2,3,4,5,6,7,8,9]
+for i in a:
+    print(i)
+
+for i in range(5,11):
+    print(i)
+    
+    
+    
---
+++ session5.py
@@ -0,0 +1,15 @@
+# program using function to perform calculation
+x=int(input("enter x value"))
+y=int(input("enter y value"))
+def add(x,y):
+    return x+y
+def sub(x,y):
+    return x-y
+def mult(x,y):
+    return x*y
+def div(x,y):
+    return x/y
+print(add(x,y))
+print(sub(x,y))
+print(mult(x,y))
+print(div(x,y))
